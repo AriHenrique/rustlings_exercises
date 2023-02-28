7cf58a4d0ec3072df9518d0943c50bf8a15efb0d
AriHenrique <arihenriqueuna@gmail.com>
2023-02-28 19:58:54 -0300

    first commit

---
+++ exercises/functions/functions2.rs
@@ -0,0 +1,15 @@
+// functions2.rs
+// Execute `rustlings hint functions2` or use the `hint` watch subcommand for a hint.
+
+
+
+fn main() {
+    call_me();
+}
+
+fn call_me(){
+    let num = 1;
+    for i in 0..num {
+        println!("Ring! Call number {}", i + 1);
+    }
+}
